670e2d0ef92fa341415a36ca6865a9868ee21c89
Mariel Hoepelman <marielhoepelman@gmail.com>
2019-09-09 16:12:58 -0400

    initial commit

---
+++ API_Key-Lab/API_Key-LabTests/API_Key_LabTests.swift
@@ -0,0 +1,34 @@
+//
+//  API_Key_LabTests.swift
+//  API_Key-LabTests
+//
+//  Created by Mariel Hoepelman on 9/9/19.
+//  Copyright © 2019 Mariel Hoepelman. All rights reserved.
+//
+
+import XCTest
+@testable import API_Key_Lab
+
+class API_Key_LabTests: XCTestCase {
+
+    override func setUp() {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
